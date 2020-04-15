6445744bd2606539b9448645ab618f02d89ee332
Tony Holroyd <tony.j.holroyd@gmail.com>
2020-04-15 09:10:37 +0100

    update

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl -X PATCH --header "Content-Type:application/json" \
+    --data '{"network": {"hostname": "'$BALENA_DEVICE_NAME_AT_INIT'"}}' \
+    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
+if [ ! -z $REMOTE_MOUNT ]
+then
+    REMOTE_DIR=/var/lib/mpd/music/Remote
+    mkdir ${REMOTE_DIR}
+    mount UUID=$REMOTE_MOUNT ${REMOTE_DIR}
+    test -x ${REMOTE_DIR}/copyover && ${REMOTE_DIR}/copyover
+fi
+
+# sed -e "s/XXXXXX/$DEVICE/" < /etc/mpd.conf.tpl > /etc/mpd.conf
+
+exec "${@}"
